2e7cf7a080224342a3efb7cc65fa9f9699d70b46
Enderson Marques <6336.cara@gmail.com>
2024-08-18 22:34:54 +0000

    test: ensure SignUpcontroller returns 400 if no password provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -6,7 +6,7 @@ describe('SignUp Controller', () => {
         const sut = new SignUpController()
         const httpRequest = {
             body: {
-                emial: 'any_email@mail.com',
+                email: 'any_email@mail.com',
                 password: 'any_password',
                 passwordConfirmation: 'any_password',
             }
@@ -28,5 +28,33 @@ describe('SignUp Controller', () => {
         const httpsResponse = sut.handle(httpRequest)
         expect(httpsResponse.statusCode).toBe(400)
         expect(httpsResponse.body).toEqual(new MissignParamnsError('email'))
+    })
+
+    test('Should return 400 if no password is provided', () => {
+        const sut = new SignUpController()
+        const httpRequest = {
+            body: {
+                email: 'any_email@mail.com',
+                name: 'any_name',
+                passwordConfirmation: 'any_password',
+            }
+        }
+        const httpsResponse = sut.handle(httpRequest)
+        expect(httpsResponse.statusCode).toBe(400)
+        expect(httpsResponse.body).toEqual(new MissignParamnsError('password'))
+    })
+
+    test('Should return 400 if no passwordConfirmation is provided', () => {
+        const sut = new SignUpController()
+        const httpRequest = {
+            body: {
+                email: 'any_email@mail.com',
+                name: 'any_name',
+                password: 'any_password',
+            }
+        }
+        const httpsResponse = sut.handle(httpRequest)
+        expect(httpsResponse.statusCode).toBe(400)
+        expect(httpsResponse.body).toEqual(new MissignParamnsError('passwordConfirmation'))
     })
 })
